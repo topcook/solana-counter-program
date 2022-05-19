f27a47cce9109f07e8891240a872a8ade3bcac11
topcook <codecooker1996@gmail.com>
2022-05-19 15:12:38 +0200

    deployed to localhost

--- tests/mysolanaapp.js
+++ tests/mysolanaapp.js
@@ -1,13 +1,56 @@
-const anchor = require("@project-serum/anchor");
+// const anchor = require("@project-serum/anchor");
 
-describe("mysolanaapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+// describe("mysolanaapp", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Mysolanaapp;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Mysolanaapp;
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+const assert = require('assert');
+const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
+
+let _baseAccount;
+
+describe('mysolanaapp', () => {
+  // create and set provider
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Mysolanaapp;
+  it('Creates a counter', async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 0: ', account.count.toString());
+    assert.ok(account.count.toString() == 0);
+    _baseAccount = baseAccount;
   });
+
+  it('Increments the counter', async () => {
+    const baseAccount = _baseAccount;
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 1: ', account.count.toString());
+    assert.ok(account.count.toString() == 1);
+  });
+
 });
